0ddfb748ca1e926c568d27c3643d9bca50869eca
MiniManch <Emaneulrokah96@gmail.com>
2023-02-04 12:57:59 +0200

    some more functionality changes

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,6 +1,14 @@
 let gridItemList = document.querySelectorAll('.grid-item');
+let display      = document.querySelector('#image-display');
 // adding mouseover event that changes tghe oopacity
 // - adding a class that is specified in css files
+
+function appear(){
+	for (let x of gridItemList){
+		x.classList.toggle('appear');
+	}
+}
+
 
 for(let i of gridItemList){
 	i.addEventListener("mouseover",function(){
@@ -8,25 +16,49 @@ for(let i of gridItemList){
 		i.classList.toggle("mouse-over-image");
 	})
 
+	// i.addEventListener("click",function(){
+	// 	console.log('click');
+	// 	console.log('this',this);
+	// 	this.classList.add('clicked');
+
+	// 	let newArr = [];
+	// 	for (let y of gridItemList){
+	// 		if (!(y.classList.toString().includes("clicked"))){
+	// 			newArr.push(y);
+	// 		}else{
+	// 			y.addEventListener('click',function(){
+	// 				y.classList.toggle('clicked');
+	// 			})
+	// 		}
+	// 	}
+	// 	console.log(newArr);
+	// 	for(let x of newArr){
+	// 		x.classList.toggle('appear');
+	// 	}
+
+	// })
+
 	i.addEventListener("click",function(){
 		console.log('click');
-		console.log('this',this);
-		this.classList.add('clicked');
+		appear();
 
-		let newArr = [];
-		for (let y of gridItemList){
-			if (!(y.classList.toString().includes("clicked"))){
-				newArr.push(y);
-			}else{
-				y.addEventListener('click',function(){
-					y.classList.toggle('clicked');
-				})
-			}
-		}
-		console.log(newArr);
-		for(let x of newArr){
-			x.classList.toggle('appear');
-		}
+		let newDiv = document.createElement('div');
+		let delBtn = document.createElement('p');
+
+		newDiv.style.backgroundImage = getComputedStyle(i, null).backgroundImage;
+		newDiv.classList.add('display-new');
+
+		delBtn.textContent = 'X';
+		delBtn.style.color = 'black';
+
+		delBtn.addEventListener('click',function(){
+			appear();
+			newDiv.parentNode.removeChild(newDiv);
+		})
+		// newDiv.addEventListener('click',fu)
+		newDiv.append(delBtn)
+		display.append(newDiv);
+
 
 	})
 
